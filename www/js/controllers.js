904c5df0ac75ba5df63f9dcc37d01bced45ae4c8
collinpartee <collin.partee@gmail.com>
2016-03-19 02:44:56 -0400

    step 2
    added images
    added avatar
    added question box

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,22 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistsCtrl', function ($scope) {
+    $scope.questionAsked = true;
+    $scope.hasFinishedPlaying = false;
+    $scope.score = 0;
+    $scope.quizLength = 3;
+    $scope.currQuizNumber = 0;
 
+
+    //    $scope.currQuestion = "none";
+    //    $scope.hasStartedPlaying = 'no';
+    //    $scope.choice1Selected = false;
+    //    $scope.choice2Selected = false;
+    //    $scope.choice3Selected = false;
+    //    $scope.choice4Selected = false;
+    //    $scope.showAvatarAnswerMessage = false;
+    //    $scope.youGotItRight = false;
+    //    $scope.questionAnswered = false;
 })
 
 .controller('PlaylistCtrl', function ($scope) {});
