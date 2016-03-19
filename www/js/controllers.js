dddae0916fd24ed51cce24824d4864573a89e151
collinpartee <collin.partee@gmail.com>
2016-03-19 02:14:15 -0400

    step 1
    clean slate

---
+++ www/js/controllers.js
@@ -0,0 +1,11 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function ($scope) {
+
+})
+
+.controller('PlaylistsCtrl', function ($scope) {
+
+})
+
+.controller('PlaylistCtrl', function ($scope) {});
