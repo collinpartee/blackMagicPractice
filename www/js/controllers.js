bc8da30113bbc0eb0c98f7e0e2a2c85b98ca595f
collinpartee <collin.partee@gmail.com>
2016-03-19 02:59:32 -0400

    STEP 3
    added answer choices
    if you select one it turns green

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,16 +11,44 @@ angular.module('starter.controllers', [])
     $scope.quizLength = 3;
     $scope.currQuizNumber = 0;
 
+    $scope.choice1Selected = false;
+    $scope.choice2Selected = false;
+    $scope.choice3Selected = false;
+    $scope.choice4Selected = false;
+
 
     //    $scope.currQuestion = "none";
     //    $scope.hasStartedPlaying = 'no';
-    //    $scope.choice1Selected = false;
-    //    $scope.choice2Selected = false;
-    //    $scope.choice3Selected = false;
-    //    $scope.choice4Selected = false;
     //    $scope.showAvatarAnswerMessage = false;
     //    $scope.youGotItRight = false;
     //    $scope.questionAnswered = false;
+
+    $scope.chooseChoice1 = function () {
+        $scope.choice1Selected = true;
+        $scope.choice2Selected = false;
+        $scope.choice3Selected = false;
+        $scope.choice4Selected = false;
+    }
+    $scope.chooseChoice2 = function () {
+        $scope.choice1Selected = false;
+        $scope.choice2Selected = true;
+        $scope.choice3Selected = false;
+        $scope.choice4Selected = false;
+    }
+    $scope.chooseChoice3 = function () {
+        $scope.choice1Selected = false;
+        $scope.choice2Selected = false;
+        $scope.choice3Selected = true;
+        $scope.choice4Selected = false;
+    }
+    $scope.chooseChoice4 = function () {
+        $scope.choice1Selected = false;
+        $scope.choice2Selected = false;
+        $scope.choice3Selected = false;
+        $scope.choice4Selected = true;
+        $scope.showAvatarAnswerMessage = true;
+    }
+
 })
 
 .controller('PlaylistCtrl', function ($scope) {});
